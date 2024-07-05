82ee2c03054a3014f366c90563d2074faa1beeff
Jazmin <163955648+jazminevendorra@users.noreply.github.com>
2024-07-05 12:23:06 -0400

    Update Midterm.java

--- Midterm.java
+++ Midterm.java
@@ -1,8 +1,7 @@
 /*
-Jazmin Even Dorra
-Midterm Exam
 
 This program will ask the user for an amount to invest, and calculate the interest rate according to the amount, and the total after one year.
+
 */
 import java.util.Scanner;
 
